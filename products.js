67b1835a3a33d33a717bf93bc6b0ce9f05213e89
Arkyadeep pal <arkyadeep.pal2017@gmail.com>
2026-02-17 17:14:05 +0530

    add products data for the Danso Infinite Product Slider

---
+++ products.js
@@ -0,0 +1,60 @@
+ const products = [
+    {
+        name: "Obisdian Puffer",
+        img: "/products/product-1.png",
+        price: 249,
+        tag: "outerwear",
+        url: "",
+    },
+    {
+        name: "Obisdian Puffer",
+        img: "/products/product-2.png",
+        price: 249,
+        tag: "outerwear",
+        url: "",
+    },
+    {
+        name: "Obisdian Puffer",
+        img: "/products/product-3.png",
+        price: 249,
+        tag: "outerwear",
+        url: "",
+    },
+    {
+        name: "Obisdian Puffer",
+        img: "/products/product-4.png",
+        price: 249,
+        tag: "outerwear",
+        url: "",
+    },
+    {
+        name: "Obisdian Puffer",
+        img: "/products/product-5.png",
+        price: 249,
+        tag: "outerwear",
+        url: "",
+    },
+    {
+        name: "Obisdian Puffer",
+        img: "/products/product-6.png",
+        price: 249,
+        tag: "outerwear",
+        url: "",
+    },
+    {
+        name: "Obisdian Puffer",
+        img: "/products/product-7.png",
+        price: 249,
+        tag: "outerwear",
+        url: "",
+    },
+    {
+        name: "Obisdian Puffer",
+        img: "/products/product-8.png",
+        price: 249,
+        tag: "outerwear",
+        url: "",
+    },
+];
+
+export default products;
